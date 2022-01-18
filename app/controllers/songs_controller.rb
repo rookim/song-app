96f458ceb3b6e8b9086057cad7296c732469f927
ro <kronew6@gmail.com>
2022-01-17 18:46:17 -0600

    creates 5 routes implementing RESTful conventions

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,14 +3,37 @@ class SongsController < ApplicationController
     render json: Song.all
   end
 
+  def create
+    song = Song.create(
+      "title": params[:title],
+      "album": params[:album],
+      "artist": params[:artist],
+      "year": params[:year],
+    )
+    render json: song
+  end
+
   def show
     id = params[:id]
     song = Song.find(id)
     render json: song
   end
 
-  def create
-    render json: params["title"]
+  def update
+    song = Song.find(params[:id])
+    song.title = params[:title] || song.title
+    song.album = params[:album] || song.album
+    song.artist = params[:artist] || song.artist
+    song.year = params[:year] || song.year
+    song.save
+
+    render json: song
   end
 
+  def destroy
+    song = Song.find(params[:id])
+    song.destroy
+
+    render json: {message: "#{song.title} has been deleted....."}
+  end
 end
